fb968da2e4bc344d19a885b8f5d2651c3a8fc79c
gangix <olcay.bilir@gmail.com>
2019-08-18 14:06:42 +0200

    Added excecutor for multithread

--- src/main/java/com/finreach/paymentservice/statistics/Statistics.java
+++ src/main/java/com/finreach/paymentservice/statistics/Statistics.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -21,35 +23,45 @@ import com.finreach.paymentservice.store.AccountService;
 
 @Service
 public class Statistics {
-	private static final int INITIAL_CAPACITY = 1000;
+	private static final int INITIAL_CAPACITY = 10;
+	private static final int INITIAL_CAPACITY_SUB = 100;
 	private static final long DURATION_MILIS = 10;
 	private static final long INITIAL_DELAY = 10_000;
 
-	public static Map<Integer, Map<String,Statistic>> STATS = new HashMap<>();
+	private static Map<Integer, Map<String,Statistic>> STATS = new ConcurrentHashMap<>(INITIAL_CAPACITY);
+	private static ExecutorService executorService = Executors.newCachedThreadPool();
 	
 	@Autowired
 	private AccountService accountService;
 	
+	
 	@Scheduled(initialDelay = INITIAL_DELAY, fixedRate = DURATION_MILIS)
 	public void prepareStatistics() {
 		List<Account> accounts = accountService.all();
-		for (int i = 1; i < 11; i++) {
-			Map<String,Statistic> secondStatistic = new ConcurrentHashMap<>(INITIAL_CAPACITY);
-			int secondFor = i;
-			for (Account account : accounts) {
-				List<Double> transactionList = account.getTransactions().stream().filter(trx -> filterTimeAndAmount(trx, secondFor))
-						.map(value -> value.getAmount()).collect(Collectors.toList());
-				if(transactionList.isEmpty()) {
-					continue;
+		executorService.execute(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 1; i < 11; i++) {
+					Map<String,Statistic> secondStatistic = new ConcurrentHashMap<>(INITIAL_CAPACITY_SUB);
+					int secondFor = i;
+					for (Account account : accounts) {
+						List<Double> transactionList = account.getTransactions().stream().filter(trx -> filterTimeAndAmount(trx, secondFor))
+								.map(value -> value.getAmount()).collect(Collectors.toList());
+						if(transactionList.isEmpty()) {
+							continue;
+						}
+						secondStatistic.put(account.getId(), new Statistic(transactionList, account.getId()));
+					}
+					if(secondStatistic.isEmpty()) {
+						if(STATS.containsKey(secondFor)) {
+							STATS.get(secondFor).clear();
+						}
+						continue;
+					}
+					STATS.put(secondFor, secondStatistic);
 				}
-				secondStatistic.put(account.getId(), new Statistic(transactionList, account.getId()));
 			}
-			if(secondStatistic.isEmpty()) {
-				STATS.put(secondFor, new ConcurrentHashMap<>(INITIAL_CAPACITY));
-				continue;
-			}
-			STATS.put(secondFor, secondStatistic);
-		}
+		});
 	}
 
 	public static void setStatistics(Map<String, Statistic> statistics, Integer second) {
